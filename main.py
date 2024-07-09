b0fecdb907097fb77dc8bb2d7906cff9ab5e39f2
Daniel Kim <kh.kim@kh-kim-mac.local>
2024-07-09 23:46:28 +0900

    error fix

--- main.py
+++ main.py
@@ -51,3 +51,7 @@ app.include_router(users_router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
