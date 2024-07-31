8ef125dd52fe8808a88e35778cf2de8ff8a52b26
KHoonKim <gangsgm@gmail.com>
2024-07-31 23:05:07 +0900

    Updated Elastic Beanstalk configuration for FastAPI

--- .platform/hooks/predeploy/01_install_dependencies.sh
+++ .platform/hooks/predeploy/01_install_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source /var/app/venv/*/bin/activate
+if [ ! -d "/var/app/venv" ]; then
+    python3 -m venv /var/app/venv
+fi
+source /var/app/venv/bin/activate
 pip install --upgrade pip
 pip install -r /var/app/staging/requirements.txt
