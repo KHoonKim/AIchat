1a13a7fd7b358f02fe4877d39a0858a039ef7064
KHoonKim <gangsgm@gmail.com>
2024-08-07 22:57:24 +0900

    test code 작성전

--- .platform/hooks/postdeploy/01_start_application.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "Activating virtual environment..."
-source /var/app/venv/bin/activate
-echo "Changing to application directory..."
-cd /var/app/current
-echo "Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:8000 main:app -k uvicorn.workers.UvicornWorker
--- .platform/hooks/predeploy/01_install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d "/var/app/venv" ]; then
-    python3 -m venv /var/app/venv
-fi
-source /var/app/venv/bin/activate
-pip install --upgrade pip
-pip install -r /var/app/staging/requirements.txt
-pip install gunicorn
